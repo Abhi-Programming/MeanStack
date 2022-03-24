5e5dc89e038b915718f0bdaef0b3647e1676cd51
Abhishek parmar <parmarabhi6@gmail.com>
2022-03-24 17:25:52 +0530

    chg

--- backend/config/globals.js
+++
@@ -1,9 +0,0 @@
-// const { serverLog, errorLog } = require('../logger');
-
-global.STATUS_CODES = require('./constants').STATUS_CODES;
-
-global.STATUS_MESSAGES = require('./constants').STATUS_MESSAGES;
-
-// global.serverLog = serverLog;
-
-// global.errorLog = errorLog;
